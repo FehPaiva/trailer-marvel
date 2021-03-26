0dab40cfcde398a239291595862a7c406721575c
Felipe Paiva <81448017+FehPaiva@users.noreply.github.com>
2021-03-26 20:14:08 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,54 @@
+var listaFilmes = ['//m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg',
+'https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg',
+'https://m.media-amazon.com/images/M/MV5BOGE4NzU1YTAtNzA3Mi00ZTA2LTg2YmYtMDJmMThiMjlkYjg2XkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_UX182_CR0,0,182,268_AL_.jpg',
+'https://m.media-amazon.com/images/M/MV5BMTYzOTc2NzU3N15BMl5BanBnXkFtZTcwNjY3MDE3NQ@@._V1_UX182_CR0,0,182,268_AL_.jpg' ,
+'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg' ,
+'https://m.media-amazon.com/images/M/MV5BMjE5MzcyNjk1M15BMl5BanBnXkFtZTcwMjQ4MjcxOQ@@._V1_UY268_CR3,0,182,268_AL_.jpg' ,
+'https://m.media-amazon.com/images/M/MV5BMTQyNzAwOTUxOF5BMl5BanBnXkFtZTcwMTE0OTc5OQ@@._V1_UY268_CR3,0,182,268_AL_.jpg' ,
+'https://m.media-amazon.com/images/M/MV5BMzA2NDkwODAwM15BMl5BanBnXkFtZTgwODk5MTgzMTE@._V1_UY268_CR1,0,182,268_AL_.jpg' ,
+'https://m.media-amazon.com/images/M/MV5BMTAwMjU5OTgxNjZeQTJeQWpwZ15BbWU4MDUxNDYxODEx._V1_UX182_CR0,0,182,268_AL_.jpg',
+'https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_UX182_CR0,0,182,268_AL_.jpg',
+'https://m.media-amazon.com/images/M/MV5BMjM2NTQ5Mzc2M15BMl5BanBnXkFtZTgwNTcxMDI2NTE@._V1_UX182_CR0,0,182,268_AL_.jpg',
+'https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_UX182_CR0,0,182,268_AL_.jpg' ,
+'https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_UX182_CR0,0,182,268_AL_.jpg' ,
+'https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_UX182_CR0,0,182,268_AL_.jpg' ,
+'https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_UX182_CR0,0,182,268_AL_.jpg',
+'https://m.media-amazon.com/images/M/MV5BMTg1MTY2MjYzNV5BMl5BanBnXkFtZTgwMTc4NTMwNDI@._V1_UX182_CR0,0,182,268_AL_.jpg',
+'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_UX182_CR0,0,182,268_AL_.jpg' ,
+'https://m.media-amazon.com/images/M/MV5BYjcyYTk0N2YtMzc4ZC00Y2E0LWFkNDgtNjE1MzZmMGE1YjY1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg',
+'https://m.media-amazon.com/images/M/MV5BMTE0YWFmOTMtYTU2ZS00ZTIxLWE3OTEtYTNiYzBkZjViZThiXkEyXkFqcGdeQXVyODMzMzQ4OTI@._V1_UX182_CR0,0,182,268_AL_.jpg',
+'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UX182_CR0,0,182,268_AL_.jpg' ]
+
+
+var linkTrailer= ['https://www.youtube.com/watch?v=8ugaeA-nMTc',
+                  'https://www.youtube.com/watch?v=BoohRoVA9WQ',
+                  'https://www.youtube.com/watch?v=JOddp-nlNvQ',
+                  'https://www.youtube.com/watch?v=7SlILk2WMTI',
+                  'https://www.youtube.com/watch?v=eOrNdBpGMv8',
+                  'https://www.youtube.com/watch?v=Ke1Y3P9D0Bc',
+                  'https://www.youtube.com/watch?v=xphJMM1ipJo',
+                  'https://www.youtube.com/watch?v=tbayiPxkUMM',
+                  'https://www.youtube.com/watch?v=d96cjJhvlMA',
+                  'https://www.youtube.com/watch?v=P5iIPfNzj2o',
+                  'https://www.youtube.com/watch?v=IEVE3KSKQ0o',
+                  'https://www.youtube.com/watch?v=dKrVegVI0Us',
+                  'https://www.youtube.com/watch?v=Lt-U_t2pUHI',
+                  'https://www.youtube.com/watch?v=dW1BIid8Osg',
+                  'https://www.youtube.com/watch?v=UvNnqWLruXA',
+                  'https://www.youtube.com/watch?v=xjDjIWPwcPU',
+                  'https://www.youtube.com/watch?v=6ZfuNTqbHE8',
+                  'https://www.youtube.com/watch?v=UUkn-enk2RU',
+                  'https://www.youtube.com/watch?v=Z1BCujX3pw8',
+                  'https://www.youtube.com/watch?v=TcMBFSGVi1c'
+
+             ]
+
+for (var i = 0; i< listaFilmes.length; i ++){
+  
+ document.write(`<a href='${linkTrailer[i]}' id="teste" target="_blank"><img src='${listaFilmes[i]}'/></a>`) 
+  
+  
+}
+
+//document.write('<img src=' + listaFilmes[i] + '>')
+
